d5c5f3823f6c58bb63d5e36f5d4d7b20eb55082e
KIM SE-KYEONG <94466572+jihodaddy@users.noreply.github.com>
2022-05-10 14:50:29 +0900

    Add files via upload

--- kafka-sample/src/main/java/kafka/kafkasample/service/KafkaProducer.java
+++ kafka-sample/src/main/java/kafka/kafkasample/service/KafkaProducer.java
@@ -1,5 +1,10 @@
 package kafka.kafkasample.service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,16 @@ public class KafkaProducer {
 
     public void sendMessage(String message) {
         System.out.printf("Produce message : %s%n", message);
-        this.kafkaTemplate.send(TOPIC, message);
+
+        Date now = new Date();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // yyyy-MM-dd HH:mm:ss
+        System.out.println(df.format(now));
+
+        JSONObject data = new JSONObject();
+        data.put("dt", df.format(now));
+        data.put("msg", message);
+
+        System.out.printf("Produce json message : %s%n", data.toJSONString());
+        this.kafkaTemplate.send(TOPIC, data.toJSONString());
     }
 }
